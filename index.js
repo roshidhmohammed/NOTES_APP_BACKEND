b449abbcca000185bc9f93349131a055d3b88c34
roshidhmohammed <roshidhmohammed777@gmail.com>
2025-06-23 00:18:24 +0530

    production vercel config file issue-4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const serverless = require("serverless-http");
-const app = require("../src/app"); // adjust path if needed
+const app = require("./src/app"); // adjust path if needed
 
 module.exports.handler = serverless(app);
